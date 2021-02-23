11b264051c0796aff790c61ef7c0aae06fb99189
Samy KACIMI <samy.kacimi@gmail.com>
2021-02-23 12:19:35 +0100

    create User & Order class

---
+++ order.rb
@@ -0,0 +1,28 @@
+class Order
+  attr_reader :id, :user_id, :direction, :btc_amount, :price
+  attr_accessor :state
+
+  def initialize(id, user_id, direction, price)
+    @id = id
+    @user_id = user_id
+    @direction = direction
+    @btc_amount = 1
+    @price = price
+    @state = 'queued'
+
+    validate!
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError.new, 'id must be a valid ID' unless positive_integer?(@id)
+    raise ArgumentError.new, 'user_id must be a valid ID' unless positive_integer?(@user_id)
+    raise ArgumentError.new, 'direction must either be "buy" or "sell"' unless @direction.in?(%w[buy sell])
+    raise ArgumentError.new, 'price must be an Integer' unless positive_integer?(@price)
+  end
+
+  def positive_integer?(data)
+    data.is_a?(Integer) && data.positive?
+  end
+end
---
+++ user.rb
@@ -0,0 +1,20 @@
+class User
+  attr_reader :id
+  attr_accessor :btc_balance, :eur_balance
+
+  def initialize(id, btc_balance, eur_balance)
+    @id = id
+    @btc_balance = btc_balance
+    @eur_balance = eur_balance
+
+    validate!
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError.new, 'id must be a valid ID' unless @id.is_a?(Integer)
+    raise ArgumentError.new, 'btc_balance must be an Integer' unless @btc_balance.is_a?(Integer)
+    raise ArgumentError.new, 'eur_balance must be an Integer' unless @eur_balance.is_a?(Integer)
+  end
+end
